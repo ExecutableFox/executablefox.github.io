6c059571afbc6fe5bc38d4f044cfc73b93bf2411
ExecutableFox <122712684+ExecutableFox@users.noreply.github.com>
2023-03-01 00:03:14 -0500

    Create function.js

---
+++ function.js
@@ -0,0 +1,3 @@
+document.getElementById('modo_noche').onclick = function(){
+    document.getElementById('todo').classList.remove("sectionflexcolumn")
+    document.getElementById('todo').classList.add("dark-mode")
