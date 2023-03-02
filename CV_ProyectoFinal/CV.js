d0ccb0d79ccc3fe70db673e75795226865bd1fee
ExecutableFox <122712684+ExecutableFox@users.noreply.github.com>
2023-03-01 19:51:03 -0500

    Delete CV_ProyectoFinal directory

--- CV_ProyectoFinal/CV.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('modo_noche').onclick = function(){
-    document.getElementById('todo').classList.remove("sectionflexcolumn")
-    document.getElementById('todo').classList.add("dark-mode")
-}
