abe226e65d781b976531a487d416bade726bd9ae
ExecutableFox <122712684+ExecutableFox@users.noreply.github.com>
2023-03-01 19:19:19 -0500

    Add files via upload

---
+++ CV_ProyectoFinal/CV.js
@@ -0,0 +1,5 @@
+
+document.getElementById('modo_noche').onclick = function(){
+    document.getElementById('todo').classList.remove("sectionflexcolumn")
+    document.getElementById('todo').classList.add("dark-mode")
+}
